cb25a6e7123e6919991a76a2c39d239d7b7b3293
Diogo-Pimenta <diogo.abcfc@gmail.com>
2019-03-14 17:29:43 -0300

    corrigindo acesso ao BD

--- application/models/Postagem.php
+++ application/models/Postagem.php
@@ -4,10 +4,10 @@
 		function get($id=null){
 			if($id){
 				$this->db->where('id', $id);
-				$query = $this->db->get('Postagem');
+				$query = $this->db->get('postagem');
 				return $query->row_array();
 			}else{
-				$query = $this->db->get('Postagem');
+				$query = $this->db->get('postagem');
 				return $query->result_array();
 			}
 		}
